e9890db4ba3a94b3be70002f6f922b50966b717b
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-07 11:21:14 +0800

    feat: custom hook extracting fetching api

--- src/features/game/game-grid.tsx
+++ src/features/game/game-grid.tsx
@@ -1,37 +1,7 @@
-import apiClient from "@/services/api-client";
-import type { AxiosError } from "axios";
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-type Game = {
-  id: number;
-  name: string;
-};
-
-type GamesResponse = {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Game[];
-};
+import useGames from "../hooks/use-games";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const control = new AbortController();
-
-    apiClient
-      .get<GamesResponse>("/games", { signal: control.signal })
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
-        if (axios.isCancel(err)) return;
-        setError((err as AxiosError).message);
-      });
-
-    return () => control.abort();
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <div>
---
+++ src/features/hooks/use-games.ts
@@ -0,0 +1,39 @@
+import apiClient from "@/services/api-client";
+import type { AxiosError } from "axios";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type Game = {
+  id: number;
+  name: string;
+};
+
+type GamesResponse = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Game[];
+};
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const control = new AbortController();
+
+    apiClient
+      .get<GamesResponse>("/games", { signal: control.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        setError((err as AxiosError).message);
+      });
+
+    return () => control.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
