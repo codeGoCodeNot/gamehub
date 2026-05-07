7deca2f1f253f06c3ee0909d21f5b97b27ec7822
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-07 10:14:50 +0800

    feat: fetch games from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/navbar/components/navbar";
+import GameGrid from "./features/game/game-grid";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen w-full">
       <Navbar />
+      <main className="py-25 px-5">
+        <GameGrid />
+      </main>
     </div>
   );
 };
---
+++ src/features/game/game-grid.tsx
@@ -0,0 +1,44 @@
+import apiClient from "@/services/api-client";
+import type { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+
+type Game = {
+  id: number;
+  name: string;
+};
+
+type GamesResponse = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Game[];
+};
+
+const GameGrid = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const control = new AbortController();
+
+    apiClient
+      .get<GamesResponse>("/games", { signal: control.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError((err as AxiosError).message));
+
+    return () => control.abort();
+  }, []);
+
+  return (
+    <div>
+      {error && <p className="text-red-500">{error}</p>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GameGrid;
