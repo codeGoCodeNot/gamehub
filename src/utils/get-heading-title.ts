901ab16b849679b82ba391810c2db865b638b11f
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-08 09:11:14 +0800

    fix: getheadin title

--- src/utils/get-heading-title.ts
+++ src/utils/get-heading-title.ts
@@ -2,7 +2,7 @@ import type { GameQuery } from "@/types/query";
 
 const getHeadingTitle = (gameQuery: GameQuery): string => {
   if (gameQuery.searchTerm)
-    return `${gameQuery.genre?.name ?? gameQuery.platform?.name}: ${gameQuery.searchTerm}`;
+    return `${gameQuery.genre?.name ?? gameQuery.platform?.name ?? "Search"}: ${gameQuery.searchTerm}`;
   if (gameQuery.genre) return gameQuery.genre.name;
   if (gameQuery.platform) return gameQuery.platform.name;
   return "All Games";
