0654457f014617f5a33b4e801e7f5a8f9d1c738a
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-10 03:09:11 +0800

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,7 @@
-import { useState } from "react";
-import Navbar from "./components/layout/navbar";
-import GameGrid from "./features/game/components/game-grid";
-import GenreList from "./features/genre/components/genre-list";
-import getHeadingTitle from "./utils/get-heading-title";
-import type { Genres } from "./features/genre/type";
-import type { Platform } from "./features/platform/type";
-import type { GameQuery, SortOrder } from "./types/query";
-import useGenres from "./features/genre/hooks/use-genres";
-import usePlatform from "./features/platform/hooks/use-platform";
+import Counter from "./learning-state-management/state-management/counter";
 
 const App = () => {
-  const [gameQuery, setGameQuery] = useState<GameQuery>({
-    genreId: undefined,
-    platformId: undefined,
-    sortOrder: "",
-    searchTerm: "",
-  });
-
-  const { data: genres } = useGenres();
-  const { data: platforms } = usePlatform();
-
-  const handleSearch = (searchTerm: string) => {
-    setGameQuery((prev) => ({ ...prev, searchTerm }));
-  };
-
-  const handleSelectedGenre = (genre: Genres | null) => {
-    setGameQuery((prev) => ({
-      ...prev,
-      genreId: prev.genreId === genre?.id ? undefined : genre?.id,
-    }));
-  };
-
-  const handleSelectedPlatforms = (platform: Platform | null) => {
-    setGameQuery((prev) => ({
-      ...prev,
-      platformId: prev.platformId === platform?.id ? undefined : platform?.id,
-    }));
-  };
-
-  const handleSelectedSortOrder = (sortOrder: SortOrder | null) => {
-    setGameQuery((prev) => ({ ...prev, sortOrder }));
-  };
-
-  return (
-    <div className="flex flex-col min-h-screen w-full">
-      <Navbar onSearch={handleSearch} />
-      <main className="flex flex-1 mt-[60px]">
-        <aside className="hidden lg:block w-64 border-r border-border h-[calc(100vh-80px)] overflow-hidden bg-background/50">
-          <GenreList
-            onGenreSelect={handleSelectedGenre}
-            selectedGenreId={gameQuery.genreId}
-          />
-        </aside>
-        <div className="flex-1 flex flex-col">
-          <GameGrid
-            gameQuery={gameQuery}
-            headingTitle={getHeadingTitle(gameQuery, genres, platforms)}
-            handleSelectedPlatforms={handleSelectedPlatforms}
-            handleSelectedSortOrder={handleSelectedSortOrder}
-          />
-        </div>
-      </main>
-    </div>
-  );
+  return <Counter />;
 };
 
 export default App;
---
+++ src/learning-state-management/state-management/counter.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { useReducer } from "react";
+import counterReducer from "./reducer/counter-reducer";
+
+const Counter = () => {
+  const [count, dispatch] = useReducer(counterReducer, 0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <Button onClick={() => dispatch({ type: "INCREMENT" })}>+</Button>
+      <Button
+        onClick={() => dispatch({ type: "DECREMENT" })}
+        disabled={count === 0}
+      >
+        -
+      </Button>
+      <Button onClick={() => dispatch({ type: "RESET" })}>Reset</Button>
+    </div>
+  );
+};
+
+export default Counter;
