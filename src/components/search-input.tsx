d0701a6e6599c83e3415444c822b509f99407e23
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-05-10 17:48:58 +0800

    refactor: useNavigate tranmsfer to serach input

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -2,16 +2,22 @@ import useGameQuery from "@/state-management/hooks/use-game-query";
 import { LucideSearch } from "lucide-react";
 import { useDebouncedCallback } from "use-debounce";
 import { Input } from "./ui/input";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const { setSearch } = useGameQuery();
+  const navigate = useNavigate();
+
+  const handleHome = () => {
+    navigate("/");
+  };
 
   const debounced = useDebouncedCallback((value) => {
     setSearch(value);
   }, 200);
 
   return (
-    <div className="w-2/3 relative">
+    <div className="w-2/3 relative" onClick={handleHome}>
       <LucideSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-4 h-4" />
       <Input
         placeholder="Search"
